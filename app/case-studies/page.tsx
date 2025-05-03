0609febfc9b06f4f62fb07379fc40f5865c50902
Flxkzy <hamza.raja29@yahoo.com>
2025-05-04 03:03:11 +0500

    updated everything again

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -24,21 +24,13 @@ export default function CaseStudiesPage() {
         </Link>
         <div className="absolute right-1/2 translate-x-1/2 transform">
           <div className="hidden md:flex gap-x-10 items-center text-gray-700 font-medium text-lg cursor-pointer">
-            <Link href="/" className="hover:text-blue-500">Home</Link>
-            <Link href="/#services" className="hover:text-blue-500">Services</Link>
-            <Link href="/#process" className="hover:text-blue-500">Process</Link>
-            <Link href="/case-studies" className="hover:text-blue-500">Case Studies</Link>
           </div>
         </div>
         <div className="flex items-center gap-x-4">
-          <a href="tel:519400200" className="hidden lg:flex">
-            <button className="px-4 py-2 rounded-md flex items-center gap-x-3 text-black">(519)-400-200</button>
-          </a>
           <Link
             href="/meeting"
-            className="py-3 px-6 text-lg rounded-[6px] border-2 border-black text-white bg-[#121212] hover:bg-[#abcbff] transition duration-200"
-          >
-            Book a call
+            className="inline-block py-2 px-6 text-sm font-medium text-white bg-[#FA7268] rounded-[6px] border-2 border-black dark:border-white transition duration-200 hover:bg-[#abcbff] hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]">
+            Book a Free Consultation
           </Link>
         </div>
       </div>
