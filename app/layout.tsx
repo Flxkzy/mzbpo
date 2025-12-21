e23a14b9e3dad9777d018dd21a9008be4680a465
Flxkzy <hamza.raja29@yahoo.com>
2025-12-22 01:57:50 +0500

    Added Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,9 +58,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
+
   return (
     <html lang="en" className={`${syne.variable} ${poppins.variable}`}>
       <head>
+        {/* Microsoft Clarity */}
         <Script
           id="microsoft-clarity"
           strategy="afterInteractive"
@@ -74,11 +77,43 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Meta Pixel */}
+        {pixelId && (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${pixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
       </head>
 
       <body className="bg-background text-foreground antialiased">
+        {/* Meta Pixel noscript */}
+        {pixelId && (
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        )}
+
         {children}
 
+        {/* Organization Schema */}
         <Script
           id="mzbpo-org-ldjson"
           type="application/ld+json"
